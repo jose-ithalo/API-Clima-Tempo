91a1377f2523a4a63b73c7726c0ce2139fb43b78
José Ithalo <jose_ithalo@hotmail.com>
2023-09-17 12:20:00 -0300

    Criação de lista para testes

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private userList: User[] = [
+    {
+      id: 1,
+      name_user: 'Jonas Moura',
+      email: 'jonas@email.com',
+      password: '123'
+    },
+    {
+      id: 2,
+      name_user: 'Rafael Ferreira',
+      email: 'rafa@email.com',
+      password: 'teste'
+    }
+  ];
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users from system`;
+    return this.userList;
   }
 
   findOne(id: number) {
