592feba39efff621d8289e0039fd23882529813d
José Ithalo <jose_ithalo@hotmail.com>
2023-09-23 10:58:12 -0300

    Substituição no db para variáveies sensíveis

--- src/dataBase/connection.ts
+++ src/dataBase/connection.ts
@@ -4,10 +4,10 @@ import connection from 'knex';
 export const knex = connection({
     client: 'pg',
     connection: {
-        host: 'localhost',
+        host: process.env.DB_HOST,
         port: 5432,
-        user: 'postgres',
-        password: 'postgres',
-        database: 'clima_arbitralis'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
 });
