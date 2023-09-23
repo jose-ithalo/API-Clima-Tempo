ce8e7ac2f1d0e20c84718796258b508df652f046
José Ithalo <jose_ithalo@hotmail.com>
2023-09-23 11:00:42 -0300

    Novo módulo do login importado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, LoginModule],
   controllers: [],
   providers: [],
 })
