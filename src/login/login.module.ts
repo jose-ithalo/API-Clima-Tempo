1f8287c4b14f0708d23a0833f15b29dc472b4ce2
José Ithalo <jose_ithalo@hotmail.com>
2023-09-24 10:19:28 -0300

    Importação do JwtModule

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
+import { JwtModule } from '@nestjs/jwt';
 import { LoginController } from './login.controller';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRETE,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [LoginController],
   providers: [LoginService],
 })
-export class LoginModule {}
+// eslint-disable-next-line prettier/prettier
+export class LoginModule { }
