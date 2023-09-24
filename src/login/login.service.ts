c125ae843344e64f545bdaa23a2951af3c7e8635
José Ithalo <jose_ithalo@hotmail.com>
2023-09-24 10:22:24 -0300

    Serviço para fazer login desenvolvido

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,13 +1,38 @@
 /* eslint-disable prettier/prettier */
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { knex } from 'src/dataBase/connection';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
 import { CreateLoginDto } from './dto/create-login.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class LoginService {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  create(createLoginDto: CreateLoginDto) {
-    return 'This action adds a new login';
+
+  constructor(private jwtService: JwtService) { }
+
+  async signIn(createLoginDto: CreateLoginDto) {
+
+    const { email, password } = createLoginDto;
+
+    const foundUser: User[] = await knex('users').where('email', email);
+
+    if (foundUser.length === 0) {
+      throw new UnauthorizedException('E-mail ou senha não confere.');
+    }
+
+    const verifyPassword: boolean = await bcrypt.compare(password, foundUser[0].password);
+
+    if (!verifyPassword) {
+      throw new UnauthorizedException('E-mail ou senha não confere.');
+    }
+
+    return {
+      access_token: await this.jwtService.signAsync({ id: foundUser[0].id })
+    };
+
   }
 
 }
