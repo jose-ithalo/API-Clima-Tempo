ff36f34792a91920375dd7effdcc16cb43ecd71b
José Ithalo <jose_ithalo@hotmail.com>
2023-10-02 19:30:31 -0300

    Função para habilitar o cors implementada

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000, (): void => {
+  await app.listen(3003, (): void => {
     console.log('Server on working...');
   });
 }
