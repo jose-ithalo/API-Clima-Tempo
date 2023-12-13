3d0b4c3d2e323995d0a09d67d4d8ae202368703e
José Ithalo <jose_ithalo@hotmail.com>
2023-12-13 09:17:41 -0300

    Configuração do MailerModule realizada

--- src/emails/emails.module.ts
+++ src/emails/emails.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { EmailsService } from './emails.service';
 import { EmailsController } from './emails.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.Email_Host,
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      },
+    }),
+  ],
   controllers: [EmailsController],
   providers: [EmailsService],
 })
-export class EmailsModule {}
+// eslint-disable-next-line prettier/prettier
+export class EmailsModule { }
