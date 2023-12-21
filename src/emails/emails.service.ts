850d701c829fce83e342e1b63607a9cf958cedb2
José Ithalo <jose_ithalo@hotmail.com>
2023-12-21 15:24:51 -0300

    Verificação de existência de email aplicada

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,15 +1,24 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create-email.dto';
 
+import { knex } from 'src/dataBase/connection';
 import { MailerService } from '@nestjs-modules/mailer';
+
+import { Email } from './entities/email.entity';
 
 @Injectable()
 export class EmailsService {
   constructor(private mailerService: MailerService) { }
 
-  sendMail(createEmailDto: CreateEmailDto): void {
+  async sendMail(createEmailDto: CreateEmailDto) {
     const { email } = createEmailDto;
+
+    const existingEmail: Email = await knex.select('email').from('users').where('email', email).first();
+
+    if (!existingEmail) {
+      throw new NotFoundException('Email não cadastrado no sistema.')
+    }
 
     this.mailerService.sendMail({
       to: email,
