29719cad335d7ba33a2e3a54a3535741cf1a8a39
José Ithalo <jose_ithalo@hotmail.com>
2023-12-21 10:58:55 -0300

    Função de envio de emails desenvolvida

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -2,14 +2,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create-email.dto';
 
+import { MailerService } from '@nestjs-modules/mailer';
+
 @Injectable()
 export class EmailsService {
-  sendMail(createEmailDto: CreateEmailDto) {
+  constructor(private mailerService: MailerService) { }
 
+  sendMail(createEmailDto: CreateEmailDto): void {
     const { email } = createEmailDto;
 
-    console.log(email);
-
-    return `This action sends a message`;
+    this.mailerService.sendMail({
+      to: email,
+      from: `Joseph Dev <${process.env.EMAIL_USER}>`,
+      subject: 'Teste de envio da API',
+      text: 'Esta mensagem foi enviada com sucesso pela API.'
+    });
   }
 }
