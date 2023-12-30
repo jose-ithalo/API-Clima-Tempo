70e402b231edf52fdd1a80a9ff40fe17f239bf17
José Ithalo <jose_ithalo@hotmail.com>
2023-12-30 11:58:33 -0300

    HTML inserido no envio de emails

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -20,11 +20,42 @@ export class EmailsService {
       throw new NotFoundException('Email não cadastrado no sistema.');
     }
 
+    const htmlBody: string = `<!DOCTYPE html>
+    <html lang="pt-br">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <title>Email</title>
+    </head>
+    
+    <body>
+        <div style="display: flex; align-items: center; flex-direction: column;
+            margin: auto; height: auto; max-width: 600px;">
+            <h1 style="color: #00008b; font-family: Verdana, Geneva; font-size: 170%; margin-top: 10px;">
+                Clima Tempo
+            </h1>
+            <p style="font-size: 120%; font-family: Roboto, sans-serif; font-weight: 400; text-align: center;">
+                Você recebeu este email porque optou pela redifinição de senha.<br />
+                Clique no botão abaixo para trocá-la
+            </p>
+            <a href="https://www.youtube.com/" target="_blank" style="margin-bottom: 20px;">
+                <button style=" background-color: #0575E6; border: transparent; border-radius: 30px; 
+                color: #fff; cursor: pointer; font-family: Roboto, sans-serif; padding: 12px 50px;">
+                    Trocar
+                </button>
+            </a>
+        </div>
+    </body>
+    
+    </html>`
+
     this.mailerService.sendMail({
       to: email,
       from: `Joseph Dev <${process.env.EMAIL_USER}>`,
       subject: 'Teste de envio da API',
-      text: 'Esta mensagem foi enviada com sucesso pela API.'
+      html: htmlBody
     });
 
     return 'Mensagem enviada!'
