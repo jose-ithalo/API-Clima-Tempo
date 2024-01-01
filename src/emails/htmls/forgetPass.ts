3116b409b09ab6a1c1a9f3021e7c351dcda26e42
José Ithalo <jose_ithalo@hotmail.com>
2023-12-31 21:41:53 -0300

    Criação de modelo HTML para nova senha

---
+++ src/emails/htmls/forgetPass.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const forgetPass: string = `<!DOCTYPE html>
+    <html lang="pt-br">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <title>Email</title>
+    </head>
+    
+    <body>
+        <div style="display: flex; align-items: center; flex-direction: column;
+            margin: auto; height: auto; max-width: 600px;">
+            <h1 style="color: #00008b; font-family: Verdana, Geneva; font-size: 170%; margin-top: 10px;">
+                Clima Tempo
+            </h1>
+            <p style="font-size: 120%; font-family: Roboto, sans-serif; font-weight: 400; text-align: center;">
+                Você recebeu este email porque optou pela redifinição de senha.<br />
+                Clique no botão abaixo para trocá-la
+            </p>
+            <a href="https://www.youtube.com/" target="_blank" style="margin-bottom: 20px;">
+                <button style=" background-color: #0575E6; border: transparent; border-radius: 30px; 
+                color: #fff; cursor: pointer; font-family: Roboto, sans-serif; padding: 12px 50px;">
+                    Trocar
+                </button>
+            </a>
+        </div>
+    </body>
+    
+    </html>`;
+
+export default forgetPass;
+
