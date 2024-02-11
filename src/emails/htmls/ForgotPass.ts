8883dcdcb5cfe5dae034b0a959885b36bdb606ac
José Ithalo <jose_ithalo@hotmail.com>
2024-02-11 09:56:31 -0300

    Alteração no HTML

--- src/emails/htmls/ForgotPass.ts
+++ src/emails/htmls/ForgotPass.ts
@@ -11,32 +11,31 @@ export class ForgotPass {
     createBody(): string {
         return `<!DOCTYPE html>
         <html lang="pt-br">
-    
+        
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
             <title>Email</title>
         </head>
-    
+        
         <body>
-            <div style="display: flex; align-items: center; flex-direction: column;
-            margin: auto; height: auto; max-width: 600px;">
-                <h1 style="color: #00008b; font-family: Verdana, Geneva; font-size: 170%; margin-top: 10px;">
+            <div style="margin: auto; height: auto; max-width: 600px; text-align: center;">
+                <h1 style="color: #00008b; font-family: Verdana, Geneva; font-size: 170%; margin-bottom: 30px;">
                     Clima Tempo
                 </h1>
-                <p style="font-size: 120%; font-family: Roboto, sans-serif; font-weight: 400; text-align: center;">
+                <p style="font-size: 120%; font-family: Roboto, sans-serif; font-weight: 400;">
                     Você recebeu este email porque optou pela redefinição de senha.<br />
                 </p>
                 <span style="color: #a18181; font-size: 100%; font-family: Roboto, sans-serif; font-weight: 600;">
                     Use esta chave para redefinir
                 </span>
-                <div style="background-color: #ddcdcd; border-radius: 12px; margin-top: 10px; padding: 18px;">
+                <div style="background-color: #ddcdcd; border-radius: 12px; margin: 10px auto; padding: 18px; width: 30%;">
                     <span style="font-family: sans-serif, monospace; font-size: 28px; letter-spacing: 3px;">
                         ${this.msgKey}
                     </span>
                 </div>
-                <p style="font-size: 120%; font-family: Roboto, sans-serif; font-weight: 400; text-align: center;">
+                <p style="font-size: 120%; font-family: Roboto, sans-serif;">
                     Clique no botão abaixo para trocar a senha
                 </p>
                 <a href="http://localhost:3000/Reset" target="_blank" style="margin-bottom: 20px;">
@@ -47,7 +46,7 @@ export class ForgotPass {
                 </a>
             </div>
         </body>
-    
+        
         </html>`;
     }
 
