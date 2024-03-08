28fc5615412e57c36e8dbc7463b1e74cd9239863
José Ithalo <jose_ithalo@hotmail.com>
2024-03-08 16:24:07 -0300

    Adição do ssl para conexão

--- src/dataBase/connection.ts
+++ src/dataBase/connection.ts
@@ -8,6 +8,9 @@ export const knex = connection({
         port: 5432,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        database: process.env.DB_DATABASE,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
